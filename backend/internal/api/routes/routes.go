ada2d2f4b6936cbf74ff744636b893582ea1835b
Aneesh <aneeshappid@gmail.com>
2025-05-09 00:05:54 +0530

    Initial Route Setup

--- backend/internal/api/routes/routes.go
+++ backend/internal/api/routes/routes.go
@@ -1,10 +1,27 @@
 package routes
 
 import (
+	"FlashQuiz/internal/api/handlers"
+	"FlashQuiz/internal/api/middleware"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func SetupRoutes(router *gin.Engine, db *gorm.DB) {
+	// Middleware
+	router.Use(middleware.LoggerMiddleware())
 
+	// Initialise Handlers
+	authHandler := handlers.NewAuthHandler(db)
+
+	// Auth Routes
+	authRoutes := router.Group("/auth")
+	{
+		authRoutes.POST("/register", authHandler.RegisterUser)
+		authRoutes.POST("/login", authHandler.Login)
+	}
+
+	protectedRoutes := router.Group("/")
+	protectedRoutes.Use(middleware.AuthMiddleware())
 }
