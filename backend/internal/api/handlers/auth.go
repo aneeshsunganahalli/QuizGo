c63f08384c7dc9aa9c6624fcd44def1843d049b9
Aneesh <aneeshappid@gmail.com>
2025-05-08 22:59:36 +0530

    RegisterUser Added

--- backend/internal/api/handlers/auth.go
+++ backend/internal/api/handlers/auth.go
@@ -3,9 +3,11 @@ package handlers
 import (
 	"FlashQuiz/internal/models"
 	"fmt"
+	"net/http"
 	"os"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"gorm.io/gorm"
 )
@@ -21,7 +23,7 @@ func NewAuthHandler(db *gorm.DB) *AuthHandler {
 // RegisterRequest -> Struct for user registration request
 type RegisterRequest struct {
 	Username string `json:"username" binding:"required,min=3,max=30"`
-	Email string `json:"email" binding:"required,email"`
+	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=8,max=100"`
 }
 
@@ -36,16 +38,16 @@ type JWTClaims struct {
 	jwt.RegisteredClaims
 }
 
-func generateJWT(user models.User) (string,error) {
+func generateJWT(user models.User) (string, error) {
 
 	secretKey := os.Getenv("JWT_SECRET_KEY")
-		fmt.Println("----JWT secret key", secretKey)
-	
+	fmt.Println("----JWT secret key", secretKey)
+
 	claims := JWTClaims{
 		Username: user.Username,
 		RegisteredClaims: jwt.RegisteredClaims{
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)), // Token expires in 24 hours
-			IssuedAt: jwt.NewNumericDate(time.Now()),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
 		},
 	}
 
@@ -53,11 +55,64 @@ func generateJWT(user models.User) (string,error) {
 	// We can create the JWT token using the claims and the secret key now
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString , err := token.SignedString([]byte(secretKey))
+	tokenString, err := token.SignedString([]byte(secretKey))
 	if err != nil {
 		return "", err
 	}
 	return tokenString, nil
 }
 
+func (h *AuthHandler) RegisterUser(c *gin.Context) {
+	var req RegisterRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return 
+	}
 
+	var existingUser models.User
+	if err := h.db.Where("username = ?", req.Username).First(&existingUser).Error; err == nil {
+		c.JSON(http.StatusConflict, gin.H{"error": "Username already exists"})
+		return 
+	}
+
+	var existingEmail models.User
+	if err := h.db.Where("email = ?", req.Email).First(&existingEmail).Error; err == nil {
+		c.JSON(http.StatusConflict, gin.H{"error": "Email already exists"})
+		return 
+	}
+
+	// Create a new user
+	user := models.User{
+		Username: req.Username,
+		Email: req.Email,
+	}
+
+	// Hash password
+	if err := user.HashPassword(req.Password); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return 
+	}
+
+	// Save user to database
+	if err := h.db.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create a user"})
+		return
+	}
+
+	// JWT Token Generation
+	token, err := generateJWT(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "User registered successfully",
+		"token": token,
+		"user" : gin.H{
+			"id": user.ID,
+			"username": user.Username,
+			"email": user.Email,
+		},
+	})
+}
